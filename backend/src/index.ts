26bbdd35679e6ace2875e71316285762819ded91
Truong Gia Huy <giahuytruong2506@gmail.com>
2025-09-22 18:49:45 +0700

    backend

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Pottery shop backend is running 🚀");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
